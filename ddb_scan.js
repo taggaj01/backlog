6a854a5ec6445740ac0fb14ba34233a63bef2d81
taggaj01 <john.taggart@bbc.co.uk>
2023-11-27 12:29:10 +0000

    working and changed for live table

--- ddb_scan.js
+++ ddb_scan.js
@@ -2,7 +2,6 @@ const { DynamoDBClient, ScanCommand, BatchWriteItemCommand } = require('@aws-sdk
 
 const client = new DynamoDBClient({});
 
-// testtable var tableName = 'url-presigner';
 var tableName = 'LiveFbdPackTrackResources-PackTrackDynamoDBTable-VZ5SB2LZNZLF';
 
 const run = async () => {
@@ -10,30 +9,49 @@ const run = async () => {
         TableName: tableName
     });
     var itemArray = [];
+    var proccessingArray = [];
+    var batchNo = 0;
+    var batchRes = 'good';
 
     const res = await client.send(params);
     for (let i = 0; i < res.Items.length; i++) {
         var item = res.Items[i];
-
         deleteReq = {
             DeleteRequest: {
-                Key: item
+                // Key: item
+                Key: { mediastore: res.Items[i].mediastore, package_id: res.Items[i].package_id }
             }
         };
-
         itemArray.push(deleteReq);
     }
 
-    const command = new BatchWriteItemCommand({
-        RequestItems: {
-            // 'url-presigner': itemArray
-            'LiveFbdPackTrackResources-PackTrackDynamoDBTable-VZ5SB2LZNZLF': itemArray
+    while (itemArray.length > 0 && batchRes === 'good') {
+        proccessingArray = itemArray.splice(0, 25);
+        console.log('Current Processsing', proccessingArray.length);
+        console.log('Items Remaining', itemArray.length);
+        console.log('Batch Number', batchNo);
+        const command = new BatchWriteItemCommand({
+            RequestItems: {
+                tableName: proccessingArray
+            }
+        });
+        const response = client.send(command, function (err, data) {
+            if (err) {
+                batchRes = 'Error';
+                console.log('Error', err);
+            } else {
+                console.log('Success', data);
+                console.log(response);
+            }
+        });
+        batchNo++;
+        if (batchNo === 100) {
+            batchRes = 'check';
+            console.log('2500 items should have been deleted. Rerun if your batch is bigger than this');
         }
-    });
+    }
 
-    const response = await client.send(command);
-    console.log(response);
-    return response;
+    console.log('Process Ended');
 };
 
 run();
